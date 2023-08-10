3a010ce262147ca7497e36bf74f2b802c020cc3c
Younggu Kang <khkkor3@naver.com>
2023-08-10 15:26:21 +0900

    lesson2.yml ADD docker-compose restart 5

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -1,9 +1,12 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 
 from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 
+
+@extend_schema(tags=["Topics"])
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
