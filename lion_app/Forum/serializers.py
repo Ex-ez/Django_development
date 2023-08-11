accf4289db949500178de6584379fe6e1d963d05
Younggu Kang <khkkor3@naver.com>
2023-08-11 15:28:01 +0900

    Forum.serializers: ADD posts to TopicSerializer

--- lion_app/Forum/serializers.py
+++ lion_app/Forum/serializers.py
@@ -21,6 +21,8 @@ class TopicSerializer(serializers.ModelSerializer):
             "updated_at",
         )
 
+    post = serializers.SerializerMethodField()
+
     def get_posts(self, obj: Topic):
         return obj.posts.all()
 
