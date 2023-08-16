f8ed660fa3d7efad92a81665531edaa228a1a8c7
Younggu Kang <khkkor3@naver.com>
2023-08-16 11:09:52 +0900

    REFACTOR: settings

---
+++ lion_app/lion_app/settings/prod.py
@@ -0,0 +1,13 @@
+from base import *
+
+SECRET_KEY = ""
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "lion-lb-18904309-980284cc24de.kr.lb.naverncp.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://lion-lb-18904309-980284cc24de.kr.lb.naverncp.com",
+]
---
+++ lion_app/lion_app/settings/staging.py
@@ -0,0 +1,13 @@
+from base import *
+
+SECRET_KEY = ""
+
+DEBUG = True
+
+ALLOWED_HOSTS = [
+    "lion-lb-staging-18975936-594b98794506.kr.lb.naverncp.com",
+]
+
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://lion-lb-staging-18975936-594b98794506.kr.lb.naverncp.com",
+# ]
