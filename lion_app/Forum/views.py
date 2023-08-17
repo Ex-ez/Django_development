5f8c47b6709d8736f5a79c34afa706a67e087a57
Younggu Kang <khkkor3@naver.com>
2023-08-17 15:49:58 +0900

    Forum: Add authorization check to topic-posts API

--- lion_app/Forum/views.py
+++ lion_app/Forum/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from .models import Topic, Post, TopicGroupUser
 from .serializers import TopicSerializer, PostSerializer
-from rest_framework.decorators import action
 
 
 @extend_schema(tags=["Topic"])
@@ -19,24 +19,19 @@ class TopicViewSet(viewsets.ModelViewSet):
         return super().create(request, *args, **kwargs)
 
     @action(detail=True, methods=["get"], url_name="posts")
-    def posts(self, request, *args, **kwargs):
+    def posts(self, request: Request, *args, **kwargs):
         topic: Topic = self.get_object()
         user = request.user
 
-        if topic.is_private:
-            qs = TopicGroupUser.objects.filter(
-                group__lte=TopicGroupUser.GroupChoices.common,
-                topic=topic,
-                user=user,
+        if not topic.can_be_access_by(user):
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data="This user is not allowed to read this topic",
             )
-            if not qs.exists():
-                return Response(
-                    status=status.HTTP_401_UNAUTHORIZED,
-                    data="This user is not allowed to read this topic",
-                )
 
         posts = topic.posts
         serializer = PostSerializer(posts, many=True)
+
         return Response(data=serializer.data)
 
 
@@ -50,17 +45,12 @@ class PostViewSet(viewsets.ModelViewSet):
         data = request.data
         topic_id = data.get("topic")
         topic = get_object_or_404(Topic, id=topic_id)
-        if topic.is_private:
-            qs = TopicGroupUser.objects.filter(
-                group__lte=TopicGroupUser.GroupChoices.common,
-                topic=topic,
-                user=user,
+
+        if not topic.can_be_access_by(user):
+            return Response(
+                status=status.HTTP_401_UNAUTHORIZED,
+                data="This user is not allowed to write a post on this topic",
             )
-            if not qs.exists():
-                return Response(
-                    status=status.HTTP_401_UNAUTHORIZED,
-                    data="This user is not allowed to write a post on this topic",
-                )
 
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
