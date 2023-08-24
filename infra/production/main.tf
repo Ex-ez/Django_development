12be995471c817c39b9abd6004c6c6b42d2de6d7
Younggu Kang <khkkor3@naver.com>
2023-08-24 15:48:25 +0900

    infra.producion: ADD loadBalancer

--- infra/production/main.tf
+++ infra/production/main.tf
@@ -7,7 +7,6 @@ terraform {
   required_version = ">= 0.13"
 }
 
-// Configure the ncloud provider
 provider "ncloud" {
   access_key  = var.NCP_ACCESS_KEY
   secret_key  = var.NCP_SECRET_KEY
@@ -16,54 +15,39 @@ provider "ncloud" {
   support_vpc = true
 }
 
-# SSH 로 서버에 접근할 때 쓰는 키 -> pem 키?
-resource "ncloud_login_key" "loginkey-prod" {
-  key_name = "test-key-prod"
+locals {
+  env = "prod"
 }
 
-## be backend server setup start
-resource "ncloud_server" "be-prod-server" {
-  subnet_no                 = ncloud_subnet.be-prod-server.id
-  name                      = "be-prod-server"
-  server_image_product_code = data.ncloud_server_products.products.server_image_product_code
-  server_product_code       = data.ncloud_server_products.products.product_code
-  login_key_name            = ncloud_login_key.loginkey-prod.key_name
-  init_script_no            = ncloud_init_script.be-prod.id
+module "network" {
+  source = "../modules/network"
 
-  network_interface {
-    network_interface_no = ncloud_network_interface.web-prod.id
-    order                = 0
-  }
-}
-resource "ncloud_public_ip" "we-prod" {
-  server_instance_no = ncloud_server.be-prod-server.id
-  description        = "public IP for backend server prod"
+  env            = local.env
+  NCP_ACCESS_KEY = var.NCP_ACCESS_KEY
+  NCP_SECRET_KEY = var.NCP_SECRET_KEY
 }
 
-output "backend_public_ip" {
-  value = ncloud_public_ip.we-prod.public_ip
-}
-## be backend server setup end
+module "servers" {
+  source = "../modules/server"
 
-## db server setup start
-resource "ncloud_server" "db-prod-server" {
-  subnet_no                 = ncloud_subnet.be-prod-server.id
-  name                      = "db-prod-server"
-  server_image_product_code = data.ncloud_server_products.products.server_image_product_code
-  server_product_code       = data.ncloud_server_products.products.product_code
-  login_key_name            = ncloud_login_key.loginkey-prod.key_name
-  init_script_no            = ncloud_init_script.db-prod.id
+  db                     = "lionforum"
+  db_user                = var.db_user
+  db_password            = var.db_password
+  django_secret_key      = var.django_secret_key
+  django_settings_module = "lion_app.settings.production"
+  NCP_ACCESS_KEY         = var.NCP_ACCESS_KEY
+  NCP_SECRET_KEY         = var.NCP_SECRET_KEY
+  password               = var.password
+  env                    = local.env
+  vpc_id                 = module.network.vpc_id
+}
 
-  network_interface {
-    network_interface_no = ncloud_network_interface.db-prod.id
-    order                = 0
-  }
+module "load_balancer" {
+  source = "../modules/loadBalancer"
+
+  env            = local.env
+  NCP_ACCESS_KEY = var.NCP_ACCESS_KEY
+  NCP_SECRET_KEY = var.NCP_SECRET_KEY
+  vpc_id         = module.network.vpc_id
+  be_instance_no = module.servers.be_instance_no
 }
-resource "ncloud_public_ip" "db-prod" {
-  server_instance_no = ncloud_server.db-prod-server.id
-  description        = "public IP for db server prod"
-}
-output "db_public_ip" {
-  value = ncloud_public_ip.db-prod.public_ip
-}
-## db server setup end
--- infra/production/variables.tf
+++ infra/production/variables.tf
@@ -1,31 +1,3 @@
-variable "db" {
-  type = string
-}
-
-variable "db_user" {
-  type = string
-}
-
-variable "db_password" {
-  type = string
-}
-
-variable "db_port" {
-  type = string
-}
-
-variable "db_host" {
-  type = string
-}
-
-variable "django_secret_key" {
-  type = string
-}
-
-variable "django_settings_module" {
-  type = string
-}
-
 variable "NCP_ACCESS_KEY" {
   type = string
 }
@@ -36,6 +8,33 @@ variable "NCP_SECRET_KEY" {
 }
 
 variable "password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_port" {
+  type    = string
+  default = "5432"
+}
+
+variable "django_settings_module" {
+  type = string
+}
+
+variable "django_secret_key" {
   type      = string
   sensitive = true
 }
